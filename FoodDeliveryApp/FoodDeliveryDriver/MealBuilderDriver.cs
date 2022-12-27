2c90ddf426033a21343fb15ffb244af5ecd90486
sudhavamsikiran damojipurapu <46421514+sudhavamsikiran@users.noreply.github.com>
2022-12-27 19:12:31 +0530

    Add files via upload

---
+++ FoodDeliveryApp/FoodDeliveryDriver/MealBuilderDriver.cs
@@ -0,0 +1,31 @@
+﻿using FoodDeliveryApp.FoodDeliveryAppModel;
+using FoodDeliveryApp.Ordering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodDeliveryApp.FoodDeliveryDriver
+{
+    public class MealBuilderDriver
+    {
+        /// <summary>
+        /// This driver called that module which followed Builder Design Pattern
+        /// </summary>
+        /// <param name="selectedMealItems"></param>
+        /// <returns></returns>
+        public double BuildMealForUser(List<FoodMenuModel> selectedMealItems)
+        {
+            Console.WriteLine();
+            Console.WriteLine("You Selected Below Meal Items");
+            Console.WriteLine("_______________________________");
+            MealBuilder mealBuilder = new MealBuilder();
+            mealBuilder.PrepareMeal(selectedMealItems);
+            mealBuilder.meal.ShowItems();
+            var totalCost = mealBuilder.meal.GetCost();
+            Console.WriteLine("Total Cost(Rs.): {0}", totalCost);
+            return totalCost;
+        }
+    }
+}
